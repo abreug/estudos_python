6907ee591a6da8c9581d75e76efc236cfaa9a6b5
Gabriel Alves de Abreu <eualvesgabriel@gmail.com>
2022-10-14 19:52:39 -0300

    Client UDP

---
+++ client_udp.py
@@ -0,0 +1,16 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+try:
+    while True:
+        msg = input("Mensagem: ") + "\n"
+        client.sendto(msg.encode(), ("127.0.0.1", 4433))
+        data, sender = client.recvfrom(1024)
+        print(sender[0] + ": " + data.decode())
+        if data.encode() == "sair" or msg =="sair\n":
+            break
+
+    client.close()    
+except Exception as error:
+    print("Erro de conexão")
+    print(error)
