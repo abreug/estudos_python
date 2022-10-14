05124bb380612d3c8fd40e649911f72bd1deda86
Gabriel Alves de Abreu <eualvesgabriel@gmail.com>
2022-10-13 21:45:00 -0300

    fisrt commit

--- client_tcp.py
+++ client_tcp.py
@@ -1,9 +1,14 @@
 import socket
 
-#trazendo informações do google atraves de uma requisição HTTP
-
+#Tempo para estabelecer a conexão
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("google.com", 80))
-client.send(b"GET / HTTP/1.1\nHost: www.google.com\n\n\n")
-pacotes_recebidos = client.recv(1024).decode()
-print(pacotes_recebidos)
+client.settimeout(1)
+
+#trazendo informações do google atraves de uma requisição HTTP
+try:
+    client.connect(("127.0.0.1", 4466))
+    client.send(b"DEU BOM")
+    pacotes_recebidos = client.recv(1024).decode()
+    print(pacotes_recebidos)
+except:
+    print("Um erro ocorreu")
